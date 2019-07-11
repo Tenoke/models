b1b13b1d4689ab8e57cb1c8c3800c1506e2c2726
Svilen Todorov <svilen@aristander.ai>
2019-07-11 15:57:14 +0200

    try

--- research/deep_contextual_bandits/bandits/algorithms/svilen.py
+++ research/deep_contextual_bandits/bandits/algorithms/svilen.py
@@ -70,7 +70,7 @@ class Svilen():
 
     def action(self, context):
         print('a-c', context)
-        return random.randint(0,self.context_dim)
+        return random.randint(0,self.hparams.context_dim-1)
 
 
 # dx = torch.Tensor(train_x).cuda()
